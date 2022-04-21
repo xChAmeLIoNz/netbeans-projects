2c4cc0ff5b4dbb630efe6dc706ad43a06507a655
xChAmeLIoNz <afogliiseppe@gmail.com>
2022-04-21 10:08:56 +0200

    uploaded latest edits

--- ProgettoCompleto/src/progettocompleto/FramePrincipale.java
+++ ProgettoCompleto/src/progettocompleto/FramePrincipale.java
@@ -2,8 +2,10 @@ package progettocompleto;
 import it.gdapplicazioni.easy.gui.*;
 import fogli.alessandro.mio.timer.MioTimer;
 import java.awt.Color;
+import java.awt.PageAttributes;
 import java.awt.event.ActionEvent;
 import javax.swing.JButton;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 public class FramePrincipale extends BasicFrame {
@@ -39,11 +41,20 @@ public class FramePrincipale extends BasicFrame {
             Color frameBackground, Object... args) {
         timerButton = createToolBarJButton("Timer", 100, 20, Color.blue, 
                 Color.WHITE, FONT_SANS_SERIF, FONT_STYLE_NORMAL, 12, CURSOR_HAND, true, true);
+        pdfButton = createToolBarJButton("Crea PDF", 100, 20, Color.RED, Color.WHITE,
+                FONT_SANS_SERIF , FONT_STYLE_NORMAL, 12, CURSOR_HAND, true, true);
+        
+        for(JComponent c : toolBarComponents) {
+            frameToolbar.add(c);
+        }
+        toolBarComponents.forEach((c)->{frameToolbar.add(c);});
     }
 
     @Override
     protected void addPanelFrameComponents(StringBuilder frameTitle, 
             JFrame parentFrame, Color frameBackground, Object... args) {
+        table = new Table(new String[]{"Nome","Cognome","Età"}, new int[]{250,250,185},
+                new int[]{TEXT_LEFT,TEXT_LEFT,TEXT_CENTER}, 0, false, SELECTION_MONO, 30, TEXT_CENTER, frameBackground, frameBackground, HEIGHT, frameBackground, frameBackground, FONT_SERIF, FONT_STYLE_BOLD, SOMEBITS, ABORT, ABORT, WIDTH, HEIGHT)
     }
 
     @Override
